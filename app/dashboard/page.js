59b0ee2b6b7d9ce76ea7963113d6190ef187a20b
Hemantsinhl <makwanahemantsinh259@gmail.com>
2026-01-27 18:50:58 +0530

    Add files via upload

---
+++ app/dashboard/page.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Dashboard from '@/components/Dashboard'
+
+const Dashboardpage = () => {
+ 
+  return (
+    <Dashboard/>
+  )
+}
+
+export default Dashboardpage
+
+export const metadata = {
+    title: "Dashboard - Get Me A Chai",
+}
+
+
