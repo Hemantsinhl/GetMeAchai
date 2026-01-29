e807fd82a9edbacd65f844d405f84c0eb18bd11e
coder7399 <makwanahemantsinh259@gmail.com>
2026-01-29 11:14:16 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,39 +3,42 @@ import GithubProvider from "next-auth/providers/github";
 import User from "@/models/User";
 import connectDb from "@/db/connectDb";
 
-const handler = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
 
-  callbacks: {
-    async signIn({ user, account }) {
-      if (account.provider === "github") {
-        await connectDb();
 
-        const currentUser = await User.findOne({ email: user.email });
+export const authoptions = NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 
-        if (!currentUser) {
-          await User.create({
-            email: user.email,
-            username: user.email.split("@")[0],
-          });
-        }
-      }
-      return true;
-    },
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account.provider == "github") {
+                await connectDb();
+                // Check if the user already exists in the database
+                const currentUser = await User.findOne({ email: email });
+                if (!currentUser) {
+                    // If not, create a new user
+                    const newUser = await User.create({
+                        email: user.email,
+                        username: user.email.split("@")[0],
+                    });
+                }
+            }
+            return true;
+        },
 
-    async session({ session }) {
-      await connectDb();
-      const dbUser = await User.findOne({ email: session.user.email });
-      session.user.name = dbUser.username;
-      return session;
-    },
-  },
-});
+        async session({ session, user, token }) {
+            await connectDb();
+            const dbUser = await User.findOne({ email: session.user.email });
+            session.user.name = dbUser.username;
+            return session;
+        },
 
-export { handler as GET, handler as POST };
+    }
+})
+
+export { authoptions as GET, authoptions as POST };
